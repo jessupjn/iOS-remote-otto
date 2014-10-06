f0d84af4ef6dbb19df171e2a99a4253797fb4dfa
Jackson Jessup <jessupjn@umich.edu>
2014-10-06 13:19:46 -0400

    added assets

--- Chives/Chives/ConnectionDoctor.swift
+++ Chives/Chives/ConnectionDoctor.swift
@@ -8,25 +8,38 @@
 
 import Foundation
 
-class ConnectionDoctor : NSObject
+class ConnectionDoctor : NSObject, NSURLConnectionDelegate, NSURLConnectionDataDelegate
 {
     private
     let CHIVESCORE1 = "53ff73065075535137281087"
     let API_KEY = "408807d68df1c7551ea6bf4858444642e19ce641"
-    
-    var receivedData : NSMutableData = NSMutableData()
+    let URL_STRING = ""
     
     internal
-    override init()
-    {
-        super.init()
-    }
-    
     func makeAPIRequest(message : NSString)
     {
+        var url = NSURL.URLWithString(URL_STRING)// Creating URL
+        var request = NSURLRequest(URL: url)// Creating Http Request
+        
+        //Making request
+        var connection = NSURLConnection(request: request, delegate: self, startImmediately: true)
+        
         println("MakeRequest: \(message)")
         
-        
+    }
+    
+    
+/* pragma mark - NSURLConnectionDataDelegate */
+    func connection(connection: NSURLConnection, didFailWithError error: NSError) {
+        println("ConnectionError: \(error)")
+    }
+    
+    func connection(connection: NSURLConnection, didReceiveResponse response: NSURLResponse) {
+        println("DidReceiveResponse")
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection) {
+        println("connectionDidFinishLoading")
     }
     
 }
